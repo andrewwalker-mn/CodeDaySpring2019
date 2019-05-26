6b2341a14fec387aabba46f6e16216c5b1b0a4af
Jeffrey Jia <jiaxx215@umn.edu>
2019-05-25 20:34:00 -0500

    Merge

--- pillowtest.py
+++ pillowtest.py
@@ -6,6 +6,52 @@ def crop(coords, start, finish):
     image = Img.open(start)
     cropped = image.crop(coords)
     cropped.save(finish)
+    #push
+
+def solvingStage(piece_list):
+    while(1):
+        coords1 = win.getMouse()
+        for piece1 in piece_list:
+            if inPiece(piece1, coords1):
+                coords2 = win.getMouse()
+                for piece2 in piece_list:
+                    if inPiece(piece2, coords2):
+                        swap(piece1, piece2)
+                        break
+                break
+        if listInOrder(Imglist):
+            puzzleDone()
+
+def listInOrder(list):
+    i = 0
+    while i < len(list) - 1:
+        if list[i] > list [i+1]:
+            return False
+        i = i+1
+    return True
+
+def puzzleDone():
+    done_message = Text(Point(SIZE_X/2, SIZE_Y/4), "Puzzle Complete!")
+    done_message.setSize(20)
+    done_message.setTextColor("white")
+    done_message.draw(win)
+
+def swap(piece1, piece2):
+    topleft1 = Point(piece1.getAnchor().getX()-piece1.getWidth()//2, piece1.getAnchor().getY()-piece1.getHeight()//2)
+    piece1x = topleft1.getX()
+    piece1y = topleft1.getY()
+    topleft2 = Point(piece2.getAnchor().getX()-piece2.getWidth()//2, piece2.getAnchor().getY()-piece2.getHeight()//2)
+    piece2x = topleft2.getX()
+    piece2y = topleft2.getY()
+    moveAnimation(piece1, (piece2x - piece1x)/20, (piece2y - piece1y)/20, 20, 0.01)
+    moveAnimation(piece2, (piece1x - piece2x)/20, (piece1y - piece2y)/20, 20, 0.01)
+    index1 = piece_list.index(piece1)
+    index2 = piece_list.index(piece2)
+    piece_list[index1] = piece2
+    piece_list[index2] = piece1
+    temp = Imglist[index1]
+    Imglist[index1] = Imglist[index2]
+    Imglist[index2] = temp
 
 def inPiece(piece, coords):
     x = coords.getX()
@@ -40,19 +86,22 @@ size_x = SIZE_X/NUM_COLS
 size_y = SIZE_Y/NUM_ROWS
 crop((0, 0, SIZE_X, SIZE_Y), image_name, "crop.gif")
 
+
 xloc = 0
 yloc = 0
 piece_count = 0
 for y in range(0, NUM_ROWS):
     for x in range(0, NUM_COLS):
-        crop((xloc, yloc, xloc + size_x, yloc + size_y), "crop.gif", "crop" + str(piece_count) + ".gif")
+        crop((xloc, yloc, xloc + size_x, yloc + size_y), image_name, "crop" + str(piece_count) + ".gif")
         xloc = xloc + size_x
         piece_count += 1
     yloc = yloc + size_y
     xloc = 0
 
 Imglist = [item for item in range(0, NUM_ROWS*NUM_COLS)]
+print(Imglist)
 shuffle(Imglist)
+print(Imglist)
 
 win = GraphWin(image_name+" puzzle", SIZE_X, SIZE_Y)
 
@@ -64,20 +113,4 @@ for i in range(0, len(Imglist)):
     piece_list[i] = Image(pt, "crop"+str(Imglist[i])+".gif")
     piece_list[i].draw(win)
 
-while (1):
-    coords1 = win.getMouse()
-    for piece1 in piece_list:
-        if inPiece(piece1, coords1):
-            coords2 = win.getMouse()
-            for piece2 in piece_list:
-                if inPiece(piece2, coords2):
-                    topleft1 = Point(piece1.getAnchor().getX()-piece1.getWidth()//2, piece1.getAnchor().getY()-piece1.getHeight()//2)
-                    piece1x = topleft1.getX()
-                    piece1y = topleft1.getY()
-                    topleft2 = Point(piece2.getAnchor().getX()-piece2.getWidth()//2, piece2.getAnchor().getY()-piece2.getHeight()//2)
-                    piece2x = topleft2.getX()
-                    piece2y = topleft2.getY()
-                    piece1.move(piece2x - piece1x, piece2y - piece1y)
-                    piece2.move(piece1x - piece2x, piece1y - piece2y)
-                    break
-            break
+solvingStage(piece_list)
